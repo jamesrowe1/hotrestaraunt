99fa9a28c21dfdbb2e0490d213c28765bd41f582
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-07-31 13:41:29 -0400

    pseudo

---
+++ server.js
@@ -0,0 +1,14 @@
+// Create the front-end (visuals) for home page, reservation form, and reservation views.
+
+//     Create a basic server using Express.JS
+
+//     Create a set of routes for getting and posting table data
+//post new reservation route
+//get old reservation route
+
+//     Create a set of routes for displaying the HTML pages
+//index route sendfile
+//reservation route sendfile
+//view tables route sendfile
+
+//     Use jQuery to run AJAX calls to GET and POST data from users to the Express server
